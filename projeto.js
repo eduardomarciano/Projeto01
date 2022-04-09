661cb2524bcdc4cbbeb72d96366461054e99d328
eduardomarciano <89046882+eduardomarciano@users.noreply.github.com>
2022-04-09 18:58:55 -0300

    oi

---
+++ projeto.js
@@ -0,0 +1,94 @@
+let users = [
+  {
+  id:1,
+  first_name:"Sengoku",
+  email:"yanyun8469@uorak.com",
+  created_at:"18/06/2017"
+},
+
+  {
+    id:2,
+    first_name:"Sakazuki",
+    email:"kostas7992@uorak.com",
+    created_at:"14/03/2014"
+},
+
+  {id:3,
+    first_name:"Kuzan",
+    email:"latinka8630@uorak.com",
+    created_at:"10/06/2021"
+  },
+
+  {id:4,
+
+    first_name:"Fujitora",
+    email:"eusiquio1440@uorak.comr",
+    created_at:"16/05/2020"
+  },
+
+  {id:5,
+    first_name:"Kizaru",
+    email:"jamika6087@uorak.com",
+    created_at:"01/11/2013"
+  },
+
+  {id:6,
+    first_name:"Tony Tony",
+    email:"andremerli74@gmail.com",
+    created_at:"06/12/2007"
+  },
+
+  {id:7,
+  first_name:"Roronoa",
+  email:"caueameni@gmail.com",
+  created_at:"  20/09/2004"
+},
+
+  {id:8,
+    first_name:"Brook",
+    email:"mswendell7@moonfruit.com",
+    created_at:"  03/04/2001"
+  },
+
+  {id:9,
+    first_name:"Monkey D.",
+    email:"bdeuzimarcorreiadesantana@gmail.com",
+    created_at:"12/01/2001 "
+  },
+
+  {id:10,
+    first_name:"Jinbe",
+    email:"etechy@tre-pr.jus.br",
+    created_at:"20/10/2021"
+  },
+
+  {id:11,
+    first_name:"Marshall D.",
+    email:"cierra3374@uorak.com",
+    created_at:"18/08/2018  "
+   },
+]
+
+
+function removerTR1(){
+	const element = document.getElementById('1');
+	element.remove();
+
+}
+function removerTR2(){
+	const element = document.getElementById('2');
+	element.remove();
+		}
+function removerTR3(){
+	const element = document.getElementById('3');
+	element.remove();
+}
+function removerTR4(){
+	const element = document.getElementById('4');
+	element.remove();
+}
+function removerTR5(){
+	const element = document.getElementById('5');
+	element.remove();
+}
+console.log(users)
